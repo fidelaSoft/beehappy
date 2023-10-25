0fa8cd90fe3163985b3b20a070d62b2ca56f066c
Matuteee720 <matijasanchez092@gmail.com>
2023-10-25 18:07:16 -0300

    Modelo Primera version

---
+++ model/insumos.py
@@ -0,0 +1,15 @@
+class Insumos:
+    def__init__(self, descripcion, cantidad, precio)
+    self.descripcion = descripcion
+    self.cantidad = cantidad
+
+i1 = Insumos("Frascos de vidrio x1kg", 8, 300.00)
+i2 = Insumos("Tapa para frascos", 8, 100.00)
+i3 = Insumos("Colmenas de abejas x6 columnas", 2, 10.000.00)
+i4 = Insumos("Tambor de miel x210lts", 0, 20.000.00)
+
+
+
+
+
+
--- model/venta.py
+++ model/venta.py
@@ -1,3 +1,9 @@
 class Venta:
-    def __init__(self, id_cliente, id_producto, total):
-        pass
+    def __init__(self, id_cliente, id_producto, cantidad, precio_unitario, total):
+        self.id_cliente = id_cliente
+        self.id_producto = id_producto
+        self.cantidad = cantidad
+        self.precio_unitario = precio_unitario
+        self.total = total
+
+
